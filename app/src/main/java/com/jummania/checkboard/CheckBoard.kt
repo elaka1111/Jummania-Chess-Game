5c9782edad963d72a952982b8f5de6f246a31a90
Jumman04 <arsharifuddinjumman04@gmail.com>
2025-03-25 23:23:47 +0600

    Refactor: Enhance CheckBoard with Chess Game Functionality
    - Update Gradle to version 8.9.1
    - Add a chess game logic to the CheckBoard view.
    - Implement onTouch to handle touch events for chess game interactions.
    - Add chess pieces to the chess board.
    - Implement piece selection and movement logic in CheckBoard.
    - Add the ability to move a piece by touching the start and end point.
    - Add red rectangle on the selection of a piece.
    - Update activity_main.xml to use updated CheckBoard view.
    - Update MainActivity.kt to handle touch events and to set listener on the CheckBoard.
    - Update deploymentTargetSelector.xml to add dialog selection for devices and emulator.

--- app/src/main/java/com/jummania/checkboard/CheckBoard.kt
+++ app/src/main/java/com/jummania/checkboard/CheckBoard.kt
@@ -6,6 +6,8 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 
@@ -18,6 +20,92 @@ class CheckBoard @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val paint = Paint()
+    private var darkColor = Color.BLACK
+    private var lightColor = Color.WHITE
+    private var chessColor: Int = 0xFF808080.toInt()
+
+    private var touchedX: Float = 0f
+    private var touchedY: Float = 0f
+
+    private val chessBoard = mutableListOf(
+        "♖️",
+        "♘️",
+        "♗️",
+        "♕️",
+        "♔️",
+        "♗️",
+        "♘️",
+        "♖️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "♙️",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♟️",
+        "♜️",
+        "♞️",
+        "♝️",
+        "♛️",
+        "♚️",
+        "♝️",
+        "♞️",
+        "♜️"
+    )
+
+
+    private val pawns = "♟️"
+
+    private var isSelected = false
+    private var selectedRowNumber = -1
+    private var selectedPiece = ""
+
+    private var isInvalidate = false
+
+    //  private var needToMove = false
+
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
@@ -27,18 +115,81 @@ class CheckBoard @JvmOverloads constructor(
         val size = min / 8
 
         var count = 0
+        var rowNumber = 0
+
+        var selected = false
 
         for (s in 0 until 8) {
-            val mY = y + (size * s)
-            val mBottom = y + size + (size * s)
+            val top = y + (size * s)
+            val bottom = top + size
 
             for (i in 0 until 8) {
-                paint.color = if (count % 2 == 0) Color.BLACK else Color.WHITE
-                canvas.drawRect(x + (size * i), mY, x + size + (size * i), mBottom, paint)
+
+                rowNumber++
+
+                val left = x + (size * i)
+                val right = left + size
+
+                if (touchedX in left..right && touchedY in top..bottom) {
+
+                    if (selectedRowNumber == rowNumber && isSelected) {
+                        isSelected = false
+                        selectedRowNumber = -1
+                        selectedPiece = ""
+                    } else if (isSelected && selectedPiece != "") {
+                        swapTo(selectedRowNumber - 1, rowNumber - 1)
+                        isSelected = false
+                        selectedRowNumber = -1
+                        isInvalidate = true
+                        selectedPiece = ""
+                        invalidate()
+                    } else if (!isInvalidate) {
+                        isSelected = true
+                        selectedRowNumber = rowNumber
+                        selectedPiece = chessBoard[rowNumber - 1]
+                    }
+
+                    Log.d("Jjj", "$isSelected, $selectedRowNumber")
+
+                    selected = true
+                }
+
+                paint.style = Paint.Style.FILL
+                paint.color = if (count % 2 == 0) darkColor else lightColor
+                // canvas.drawRect(left, top, right, bottom, paint)
+
+
+                if (selectedRowNumber == rowNumber && selected && isSelected) {
+                    Log.d("Jjj", "$selectedRowNumber")
+                    paint.style = Paint.Style.STROKE
+                    val padding = 11f
+                    paint.strokeWidth = padding
+                    paint.color = Color.RED
+
+                    canvas.drawRect(
+                        left + padding, top + padding, right - padding, bottom - padding, paint
+                    )
+                }
+
+                fun drawPieces(text: String) {
+                    drawText(
+                        canvas,
+                        chessColor,
+                        size * 0.6f,
+                        text,
+                        left + (size / 2),
+                        top + (size / 1.5f)
+                    )
+                }
+
+                drawPieces(chessBoard[rowNumber - 1])
+
                 count += 1
             }
             count += 1
         }
+
+        if (!selected) isSelected = false
 
         // Draw the border
         paint.color = Color.BLACK
@@ -50,14 +201,38 @@ class CheckBoard @JvmOverloads constructor(
 
         canvas.drawRect(x, y, startX, startY, paint)
 
-        // Draw title
+        drawText(canvas, Color.BLACK, 60f, "Chess Game", width / 2f, y - 30)
+    }
+
+    private fun drawText(
+        canvas: Canvas, color: Int, textSize: Float, text: String, x: Float, y: Float
+    ) {
         paint.style = Paint.Style.FILL
         paint.typeface = Typeface.DEFAULT_BOLD
-        paint.textSize = 55f
-        paint.color = Color.BLACK
+        paint.textSize = textSize
+        paint.color = color
         paint.textAlign = Paint.Align.CENTER
-
-        canvas.drawText("Check Board Pattern", width / 2f, y - 30, paint)
+        canvas.drawText(text, x, y, paint)
     }
+
+    fun onTouch(event: MotionEvent) {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            touchedX = event.x
+            touchedY = event.y
+            invalidate()
+            isInvalidate = false
+        }
+    }
+
+    private fun swapTo(fromIndex: Int, toIndex: Int) {
+        if (fromIndex in chessBoard.indices && toIndex in chessBoard.indices) {
+            val fromPiece = chessBoard[fromIndex]
+            if (fromPiece != "") {
+                chessBoard[toIndex] = fromPiece
+                chessBoard[fromIndex] = ""
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/jummania/checkboard/MainActivity.kt
+++ app/src/main/java/com/jummania/checkboard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jummania.checkboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +8,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val board = findViewById<CheckBoard>(R.id.board)
+        ViewCompat.setOnApplyWindowInsetsListener(board) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        board.setOnTouchListener { _, event ->
+            board.onTouch(event)
+            true
         }
     }
 }
